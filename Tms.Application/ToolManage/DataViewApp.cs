e0ce6014d81df4ea2d14cb176f254f83a21baf7d
tushengtao <1450190944@qq.com>
2020-05-12 18:52:48 +0800

    dataView相关

---
+++ Tms.Application/ToolManage/DataViewApp.cs
@@ -0,0 +1,36 @@
+﻿using Tms.Code;
+using Tms.Domain.Entity.ToolManage;
+using Tms.Domain.IRepository.ToolManage;
+using Tms.Repository.ToolManage;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace Tms.Application.ToolManage
+{
+    public class DataViewApp
+    {
+
+
+        private IDataView service = new DataViewRepository();
+        // 获取所有
+        public List<DataViewEntity> GetList()
+        {
+
+            return service.IQueryable().ToList();
+
+        }
+        public List<DataViewEntity> GetList(Pagination pagination, string keyword)
+        {
+            var expression = ExtLinq.True<DataViewEntity>();
+            if (!string.IsNullOrEmpty(keyword))
+            {
+                expression = expression.And(t => t.T_Id.ToString().Contains(keyword));
+                expression = expression.Or(t => t.T_TypeName.Contains(keyword));
+                expression = expression.Or(t => t.T_DepartmentId.Contains(keyword));
+            }
+         /*   expression = expression.And(t => t.F_Account != "admin");*/
+            return service.FindList(expression, pagination);
+        }
+
+    }
+}
---
+++ Tms.Repository/ToolManage/DataViewRepository.cs
@@ -0,0 +1,20 @@
+﻿
+
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Text;
+using Tms.Data;
+using Tms.Domain.Entity.ToolManage;
+using Tms.Domain.IRepository.ToolManage;
+
+
+namespace Tms.Repository.ToolManage
+{
+    public class DataViewRepository : RepositoryBase<DataViewEntity>, IDataView
+    {
+        
+       
+    }
+}
