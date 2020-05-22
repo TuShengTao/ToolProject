28b3c7e0590ac6e3dbf670aef9020a64c02bcbf5
tushengtao <1450190944@qq.com>
2020-05-22 09:26:22 +0800

    增加Location的map

---
+++ Tms.Mapping/ToolManage/LocationMap.cs
@@ -0,0 +1,16 @@
+﻿
+
+using Tms.Domain.Entity.ToolManage;
+using System.Data.Entity.ModelConfiguration;
+
+namespace Tms.Mapping.ToolManage
+{
+    public class LocationMap : EntityTypeConfiguration<LocationEntity>
+    {
+        public LocationMap()
+        {
+            this.ToTable("Tools_Location");
+            this.HasKey(t => t.T_Id);
+        }
+    }
+}
